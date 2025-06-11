b49caf49cc831dbfd2a3abcd93b91736f8772546
Yanuu <yuhtukutoprak999@gmail.com>
2025-06-11 19:19:27 +0700

    update

--- api/index.py
+++ api/index.py
@@ -1,4 +1,35 @@
-from app import app
+from flask import Flask, request, jsonify
+import pandas as pd
+import joblib
+
+app = Flask(__name__)
+model = joblib.load("manual_password_model.pkl")
+
+def extract_features(password):
+    return pd.DataFrame([{
+        "length": len(password),
+        "digits": sum(c.isdigit() for c in password),
+        "uppers": sum(c.isupper() for c in password),
+        "lowers": sum(c.islower() for c in password),
+        "symbols": sum(not c.isalnum() for c in password)
+    }])
+
+@app.route('/')
+def home():
+    return "API aktif!"
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    password = data.get("password", "")
+    if not password:
+        return jsonify({"error": "Password is required"}), 400
+
+    features = extract_features(password)
+    strength = model.predict(features)[0]
+    return jsonify({"strength": strength})
+
+# Adapt Flask to Vercel
 from vercel_wsgi import handle_request
 
 def handler(environ, start_response):
