46dd27ee142024a954a1eb5cb1e12d55c749a754
Yanuu <yuhtukutoprak999@gmail.com>
2025-06-11 19:35:50 +0700

    update

--- api/index.py
+++ api/index.py
@@ -28,9 +28,3 @@ def predict():
     features = extract_features(password)
     strength = model.predict(features)[0]
     return jsonify({"strength": strength})
-
-# Adapt Flask to Vercel
-from vercel_wsgi import handle_request
-
-def handler(environ, start_response):
-    return handle_request(app, environ, start_response)
